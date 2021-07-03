2b74fe360175900700529f28612bf35f8e4f640e
Vadym Kochan <vadim4j@gmail.com>
2021-07-03 14:52:35 +0300

    tests: dummy: call dummy func with string arg
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- tests/plugins/dummy/dummy.c
+++ tests/plugins/dummy/dummy.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int dummy2_sum(int x, int y);
+void dummy2_print(const char *msg);
+
 
 int dummy_on_init(void)
 {
 	printf("Dummy plugin is loaded\n");
 	printf("call dummy2_sum(2,7)=%d\n", dummy2_sum(2,7));
+	dummy2_print("hello");
 	return 0;
 };
 
--- tests/plugins/dummy2/dummy2.c
+++ tests/plugins/dummy2/dummy2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+void dummy2_print(const char *msg)
+{
+	printf("msg: %s\n", msg);
+}
+
 int dummy2_sum(int x, int y)
 {
 	return x + y;
